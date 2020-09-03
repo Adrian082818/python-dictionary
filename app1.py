c9c808c32beaee527092d646d56ff466da47ab8a
Adrian082818 <mohabiradrian@gmail.com>
2020-09-03 13:18:32 -0400

    will add to conditional for catching  miss spelled words

--- app1.py
+++ app1.py
@@ -1,14 +1,17 @@
-import json
+import json #importing libraries
+from difflib import get_close_matches
 
-data = json.load(open("data/data.json"))
+data = json.load(open("data/data.json")) #loading data in python into a python datatype
 
 def translate(w):
     w = w.lower()
     if w in data:        
         return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
     else:
         return "That word doesn't exist, Please double check it."
 
-word = input("Enter word: ")
+word = input("Enter word: ") #user input 
 
 print(translate(word))
